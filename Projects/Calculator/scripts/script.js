4ce99760ccbdae48c0dfde7b95a73ceb56f3bfda
unknown <charlesquides03@gmail.com>
2023-09-02 01:12:28 +0800

    Fifth Commit

--- Projects/Calculator/scripts/script.js
+++ Projects/Calculator/scripts/script.js
@@ -2,6 +2,14 @@ console.log('08.25.23')
     
 let pampers = [];
 let x = 0;
+let sym='';
+let dotCounter = 0;
+let num1 = null;
+let num2 = '';
+
+let expression;
+let result;
+let resChecker;
 
 function nums(num){
   if(true){
@@ -14,47 +22,85 @@ function nums(num){
       } else {
         if(x === 0){          
           document.querySelector('#answer').innerHTML = num;
+          document.querySelector('#value').innerHTML = num;
           x++;
           pampers = document.querySelector('#answer').innerHTML.split('');
+          
+          if(num === '.'){ dotCounter += 1; }
         } else {
-          document.querySelector('#answer').innerHTML += num;
-          pampers = document.querySelector('#answer').innerHTML.split('');
+          if(num === '.') { dotCounter += 1; }
+
+          if(dotCounter === 2){
+            alert("You already clicked the dot symbol!");
+            dotCounter -= 1;
+          } else {
+            document.querySelector('#answer').innerHTML += num;
+            document.querySelector('#value').innerHTML += num;
+            pampers = document.querySelector('#answer').innerHTML.split('');
+          }
         }
       }
     }
   } else {
-      alert("Choose an operator symbol first!");
+    alert("Choose an operator symbol first!");
   }
 }
 
 function sign(symbol){
+  sym=symbol;
+  console.log(symbol);
   if(pampers.length  > 11){
     alert("You have reached the limit");
   } else {
-    if(pampers[pampers.length-1] === '+' || pampers[pampers.length-1] === '-' || pampers[pampers.length-1] === '*' || pampers[pampers.length-1] === '/'){
+    if(pampers[pampers.length-1] === '+' || pampers[pampers.length-1] === '-' || pampers[pampers.length-1] === '*' || pampers[pampers.length-1] === '/' || pampers[pampers.length-1] === '^' || pampers[pampers.length-1] === '√' || pampers[pampers.length-1] === 'o' || pampers[pampers.length-1] === 'c'){
       alert("Choose a number first");
     } else {
+      if(symbol === 'o') {symbol = '('}
+      if(symbol === 'c') {symbol = ')'}
+
+      if(!num1) {num1 = document.querySelector('#value').innerHTML;}
       document.querySelector('#answer').innerHTML += symbol;
       pampers = document.querySelector('#answer').innerHTML.split('');
+      document.querySelector('#value').innerHTML = '';
+      dotCounter = 0;
+      if(symbol === '-' || symbol === '√')
+      x++;
     }
   }
 }
 
 function calculate() {
-  let expression = document.getElementById("answer").innerHTML;
-  let result = eval(expression);
-  let resChecker = result.toString().split('')
-  
-  if(resChecker.length > 12){
-    console.log(resChecker.length);
-    resChecker.splice(12, 9);
-    console.log(resChecker.length);
-    document.getElementById("answer").innerHTML = resChecker.join('');      
-    pampers = document.querySelector('#answer').innerHTML.split('');
-  } else {
+  if(sym === '√'){
+    result = Math.sqrt(parseFloat(num2));
+      
     console.log(resChecker.length);
     document.getElementById("answer").innerHTML = result;      
     pampers = document.querySelector('#answer').innerHTML.split('');
+    num1 = result;
+  } else {
+    console.log(sym);
+    expression = document.getElementById("answer").innerHTML;
+    result = eval(expression);
+    resChecker = result.toString().split('');
+    
+    num2 = document.querySelector('#value').innerHTML;
+    console.log(num1, num2)
+    if(resChecker.length > 12){
+      console.log(resChecker.length);
+      resChecker.splice(12, 9);
+      console.log(resChecker.length);
+      document.getElementById("answer").innerHTML = resChecker.join('');      
+      pampers = document.querySelector('#answer').innerHTML.split('');
+    } else {
+      if(sym === '^'){
+        result = Math.pow(parseFloat(num1), parseFloat(num2));
+      }
+
+      console.log(resChecker.length);
+      document.getElementById("answer").innerHTML = result;      
+      pampers = document.querySelector('#answer').innerHTML.split('');
+      num1 = result;
+    }
   }
 }
 
@@ -62,6 +108,7 @@ function clearBtn() {
   x = 0;
   pampers = [];
   document.querySelector('#answer').innerHTML = '&nbsp;';
+  num1 = null;
 }
 
 function backSpace() {
@@ -70,7 +117,7 @@ function backSpace() {
   if(pampers.length === 0){
     document.querySelector('#answer').innerHTML = '&nbsp;';
     pampers=[];
-  } else {   
+  } else {
     document.querySelector('#answer').innerHTML = pampers.join('');
   }
 }
