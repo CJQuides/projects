fe66f04bc54f9fb8b016a4a9429b2b02145de4d3
unknown <charlesquides03@gmail.com>
2023-10-05 02:56:02 +0800

    21th Scroll To Top button better

--- Projects/Covid-19/scripts/script.js
+++ Projects/Covid-19/scripts/script.js
@@ -1,18 +1,14 @@
-// Get the button element
-var scrollToTopButton = document.querySelector('.gotopbtn');
+var gotopbtn = document.querySelector('.gotopbtn');
 
-// Add a scroll event listener to show/hide the button
 window.addEventListener('scroll', function() {
     if (document.documentElement.scrollTop > 500) {
-        scrollToTopButton.style.display = 'block';
+        gotopbtn.classList.add('show');
     } else {
-        scrollToTopButton.style.display = 'none';
+        gotopbtn.classList.remove('show');
     }
 });
 
-// Add a click event listener to scroll to the top when the button is clicked
-scrollToTopButton.addEventListener('click', function() {
-    // Scroll to the top of the page smoothly
+gotopbtn.addEventListener('click', function() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
